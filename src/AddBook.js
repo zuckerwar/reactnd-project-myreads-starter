b80352ce23fe42000b4a151efaf033a3e1aac34b
zuckerwar <36587988+zuckerwar@users.noreply.github.com>
2018-08-12 11:21:40 +0200

    feat: update App

--- src/AddBook.js
+++ src/AddBook.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
-import BookShelf from './BookShelf'
+import BookShelf from './BooksShelf'
 
 class AddBook extends Component {
   static propTypes = {
--- src/BooksShelf.js
+++ src/BooksShelf.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
+import Book from './Book'
 
 class BooksShelf extends Component {
   static PropTypes = {
