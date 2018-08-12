d1264b770644c3dc6d8bda2d9e7ac2d34022d08a
zuckerwar <36587988+zuckerwar@users.noreply.github.com>
2018-08-12 12:50:43 +0200

    fix: search when a book doesn't have a thumbnail or an author

--- src/Book.js
+++ src/Book.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import ListBook from './ListBook'
+import noCover from './icons/no-cover-image.png'
 
 
 class Book extends Component {
@@ -13,19 +14,21 @@ class Book extends Component {
 
   render() {
     const { book, books, changeShelf } = this.props
+    const coverImg = book.imageLinks && book.imageLinks.thumbnail ? book.imageLinks.thumbnail : noCover
+    const title = book.title ? book.title : "No title available"
 
     return (
           <li>
             <div className="book">
               <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${coverImg}")` }}>
               </div>
               <ListBook
                 book={ book }
                 books={ books }
                 changeShelf={ changeShelf } />
             </div>
-            <div className="book-title">{ book.title }</div>
+            <div className="book-title">{ title }</div>
             {
               book.authors && book.authors.map((author, index) => (
                 <div className="book-authors" key={index}>{author}</div>
