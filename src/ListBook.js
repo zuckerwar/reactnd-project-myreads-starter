ab86fffd18dcfe5e67cd32b6a30c7bd5643706fe
zuckerwar <36587988+zuckerwar@users.noreply.github.com>
2018-08-13 21:06:38 +0200

    fix: listed book none  bug

--- src/ListBook.js
+++ src/ListBook.js
@@ -24,7 +24,7 @@ class ListBook extends Component {
       <div className="book-shelf-changer">
         <select onChange={(event) => changeShelf(book, event.target.value)}
           defaultValue={ currentShelf }>
-          <option value="none" disabled>Move to...</option>
+          <option value="MoveTo" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
